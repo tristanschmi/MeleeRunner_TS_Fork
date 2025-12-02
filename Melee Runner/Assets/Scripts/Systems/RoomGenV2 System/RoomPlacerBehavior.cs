105ca53641b8adfb5dbaab9e3eb696d5d4bc6890
Zackery Elmer <Zackeryelmer@comcast.net>
2025-12-02 10:43:14 -0700

    Added Windy Room, Added Collisions to all rooms

--- Melee Runner/Assets/Scripts/Systems/RoomGenV2 System/RoomPlacerBehavior.cs
+++ Melee Runner/Assets/Scripts/Systems/RoomGenV2 System/RoomPlacerBehavior.cs
@@ -183,7 +183,7 @@ public class RoomPlacerBehavior : MonoBehaviour
         }
 
 
-        gridOccupancy.visualizeCells(areaCellSpaces); // See Door Spaces
+        // gridOccupancy.visualizeCells(areaCellSpaces); // See Door Spaces
 
         if (gridOccupancy.doCellsOverlap(lastRoomCells, areaCellSpaces)) { Debug.LogWarning("Door Ovelap With Previous Room"); return false; }
 
